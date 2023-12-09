93a8ab4fd4e1bf6d51b0d57b340090714f3d0f52
Rygno <alfred@rygno.net>
2023-12-09 11:42:01 +0100

    day 9

---
+++ src/bin/9b.rs
@@ -0,0 +1,42 @@
+use aoc23::read_stdin_to_string;
+use chumsky::prelude::*;
+use itertools::Itertools;
+
+fn parser() -> impl Parser<char, Vec<Vec<i64>>, Error = Simple<char>> {
+    let int = just('-')
+        .or_not()
+        .chain::<char, _, _>(filter(|c: &char| c.is_ascii_digit()).repeated().at_least(1))
+        .collect::<String>()
+        .from_str::<i64>()
+        .unwrapped()
+        .padded_by(just(' ').ignored().repeated());
+    let seq = int.repeated().at_least(1).padded();
+    seq.repeated().then_ignore(end())
+}
+
+fn extrapolate(mut seq: Vec<i64>) -> i64 {
+    let mut stack = Vec::new();
+    while !seq.iter().all(|&x| x == 0) {
+        let new = seq.iter().tuple_windows().map(|(x, y)| y - x).collect_vec();
+        stack.push(seq);
+        seq = new;
+    }
+
+    stack
+        .into_iter()
+        .rev()
+        .fold(0, |below, seq| below + seq.last().unwrap())
+}
+
+fn main() {
+    let seqs = parser().parse(read_stdin_to_string()).unwrap();
+    let result = seqs
+        .into_iter()
+        .map(|mut seq| {
+            seq.reverse();
+            seq
+        })
+        .map(extrapolate)
+        .sum::<i64>();
+    println!("{result}");
+}
