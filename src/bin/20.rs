aa283cd6885755e337c82809da4ecb9b358514d0
Rygno <alfred@rygno.net>
2023-12-21 23:02:46 +0100

    finish day 20

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,9 +1,18 @@
-use std::{collections::VecDeque, fmt, ops::Not};
+use std::{
+    collections::{BTreeMap, BTreeSet, VecDeque},
+    fmt,
+    ops::Not,
+};
 
 use aoc23::read_stdin_to_string;
+use graphviz_rust::{
+    dot_generator::*,
+    dot_structures::*,
+    printer::{DotPrinter, PrinterContext},
+};
+use itertools::Itertools;
 use rustc_hash::FxHashMap;
 use strum::EnumIs;
-use vecmap::VecMap;
 use winnow::{
     ascii::{line_ending, space0},
     combinator::{alt, preceded, repeat, separated},
@@ -14,7 +23,7 @@ use winnow::{
 
 type Tag<'a> = &'a str;
 
-#[derive(Debug, Clone, Copy, EnumIs)]
+#[derive(Debug, Clone, Copy, EnumIs, PartialEq, Eq, Hash)]
 enum PulseValue {
     Low,
     High,
@@ -43,35 +52,37 @@ impl Not for PulseValue {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Module<'a> {
     Broadcaster,
     FlipFlop(PulseValue),
-    Conjunction(VecMap<Tag<'a>, PulseValue>),
+    Conjunction(BTreeMap<Tag<'a>, PulseValue>),
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pulse<'a> {
     source: Tag<'a>,
     target: Tag<'a>,
     value: PulseValue,
+    depth: u32,
 }
 
-#[derive(Debug, Clone)]
-struct Node<'a> {
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct ModuleWithOutputs<'a> {
     module: Module<'a>,
     outputs: Vec<Tag<'a>>,
 }
 
 const DEBUG: bool = false;
 
-fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
-    let mut graph = FxHashMap::default();
+type ModGraph<'a> = BTreeMap<&'a str, ModuleWithOutputs<'a>>;
 
+fn build_graph<'a>(input: &'a [(Tag, Module, Vec<Tag>)]) -> ModGraph<'a> {
+    let mut graph = ModGraph::default();
     for (tag, module, outputs) in input {
         graph.insert(
-            tag,
-            Node {
+            *tag,
+            ModuleWithOutputs {
                 module: module.clone(),
                 outputs: outputs.clone(),
             },
@@ -80,7 +91,7 @@ fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
 
     for (tag, _, outputs) in input {
         for output in outputs {
-            if let Some(Node {
+            if let Some(ModuleWithOutputs {
                 module: Module::Conjunction(inputs),
                 ..
             }) = graph.get_mut(output)
@@ -89,6 +100,69 @@ fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
             }
         }
     }
+    graph
+}
+
+fn resolve_pulse<'a>(
+    source: Tag<'a>,
+    target: Tag<'a>,
+    value: PulseValue,
+    graph: &mut ModGraph<'a>,
+    queue: &mut VecDeque<Pulse<'a>>,
+    mut on_pulse: impl FnMut(Pulse<'a>),
+) {
+    macro_rules! pulse {
+        ($source:expr, $value:expr, $depth:expr) => {{
+            let (source, value, depth) = ($source, $value, $depth);
+            for target in &graph[&source].outputs {
+                queue.push_back(Pulse {
+                    source,
+                    target,
+                    value,
+                    depth,
+                });
+            }
+        }};
+    }
+
+    queue.push_back(Pulse {
+        source,
+        target,
+        value,
+        depth: 0,
+    });
+
+    while let Some(pulse) = queue.pop_front() {
+        if DEBUG {
+            println!("{} -{}-> {}", pulse.source, pulse.value, pulse.target);
+        }
+
+        on_pulse(pulse);
+
+        if let Some(node) = graph.get_mut(&pulse.target) {
+            match node.module {
+                Module::Broadcaster => pulse!(pulse.target, pulse.value, pulse.depth + 1),
+                Module::FlipFlop(ref mut state) => {
+                    if pulse.value.is_low() {
+                        *state = !*state;
+                        pulse!(pulse.target, *state, pulse.depth + 1);
+                    }
+                }
+                Module::Conjunction(ref mut inputs) => {
+                    inputs.insert(pulse.source, pulse.value);
+                    if inputs.iter().all(|(_, &input)| input.is_high()) {
+                        pulse!(pulse.target, PulseValue::Low, pulse.depth + 1);
+                    } else {
+                        pulse!(pulse.target, PulseValue::High, pulse.depth + 1);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
+    let mut graph = build_graph(input);
 
     if DEBUG {
         println!("{graph:#?}");
@@ -97,58 +171,18 @@ fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
     let mut queue = VecDeque::new();
     let (mut n_low, mut n_high) = (0, 0);
 
-    macro_rules! pulse {
-        ($source:expr, $value:expr) => {{
-            let source = $source;
-            let value = $value;
-            for target in &graph[&source].outputs {
-                queue.push_back(Pulse {
-                    source,
-                    target,
-                    value,
-                });
-            }
-        }};
-    }
-
     for _ in 0..1000 {
-        if DEBUG {
-            println!("button -low-> broadcaster");
-        }
-        n_low += 1;
-
-        pulse!("broadcaster", PulseValue::Low);
-
-        while let Some(pulse) = queue.pop_front() {
-            if DEBUG {
-                println!("{} -{}-> {}", pulse.source, pulse.value, pulse.target);
-            }
-
-            match pulse.value {
+        resolve_pulse(
+            "button",
+            "broadcaster",
+            PulseValue::Low,
+            &mut graph,
+            &mut queue,
+            |pulse| match pulse.value {
                 PulseValue::Low => n_low += 1,
                 PulseValue::High => n_high += 1,
-            };
-
-            if let Some(node) = graph.get_mut(&pulse.target) {
-                match node.module {
-                    Module::Broadcaster => pulse!(pulse.target, pulse.value),
-                    Module::FlipFlop(ref mut state) => {
-                        if pulse.value.is_low() {
-                            *state = !*state;
-                            pulse!(pulse.target, *state);
-                        }
-                    }
-                    Module::Conjunction(ref mut inputs) => {
-                        inputs[pulse.source] = pulse.value;
-                        if inputs.iter().all(|(_, &input)| input.is_high()) {
-                            pulse!(pulse.target, PulseValue::Low);
-                        } else {
-                            pulse!(pulse.target, PulseValue::High);
-                        }
-                    }
-                }
-            }
-        }
+            },
+        );
     }
 
     if DEBUG {
@@ -158,11 +192,143 @@ fn solve_a(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
     n_low * n_high
 }
 
+fn solve_b(input: &[(Tag, Module, Vec<Tag>)]) -> u64 {
+    let graph = build_graph(input);
+    let starts = graph["broadcaster"].outputs.iter().copied().collect_vec();
+    let end = "hb";
+
+    let mut periods = Vec::new();
+
+    for start in starts {
+        dbg!(start);
+
+        let mut partition = BTreeSet::new();
+        partition.insert(start);
+
+        let mut new = Vec::<&str>::new();
+        loop {
+            new.extend(
+                partition
+                    .iter()
+                    .flat_map(|&tag| graph[tag].outputs.iter())
+                    .filter(|&&neighbor| !partition.contains(&neighbor) && neighbor != end),
+            );
+            if new.is_empty() {
+                break;
+            }
+            partition.extend(new.drain(..));
+        }
+
+        dbg!(&partition);
+
+        let mut graph = graph
+            .iter()
+            .filter(|(tag, _)| partition.contains(*tag))
+            .map(|(tag, module)| (*tag, module.clone()))
+            .collect::<ModGraph>();
+
+        let mut seen = FxHashMap::default();
+        let mut queue = VecDeque::new();
+        let mut pattern = Vec::new();
+        let period_start = loop {
+            match seen.insert(graph.clone(), seen.len()) {
+                None => (),
+                Some(old) => break old,
+            }
+
+            let mut pulses = Vec::new();
+            resolve_pulse(
+                "broadcaster",
+                start,
+                PulseValue::Low,
+                &mut graph,
+                &mut queue,
+                |pulse| {
+                    if pulse.target == end {
+                        pulses.push((pulse.value, pulse.depth));
+                    }
+                },
+            );
+
+            pattern.push(pulses);
+        };
+
+        let (high_i, high_v) = pattern
+            .into_iter()
+            .enumerate()
+            .filter(|(_, v)| v.iter().any(|(value, _)| value.is_high()))
+            .exactly_one()
+            .unwrap();
+
+        assert!(high_v.len() > 1);
+
+        let (_, high_depth) = high_v
+            .into_iter()
+            .filter(|(value, _)| value.is_high())
+            .exactly_one()
+            .unwrap();
+
+        let period = seen.len() - period_start;
+
+        dbg!(period_start);
+        dbg!(period);
+        dbg!(high_i);
+        dbg!(high_depth);
+
+        assert_eq!(period_start, 1);
+        assert_eq!(high_depth, 3);
+        assert_eq!(high_i, period - 1);
+
+        periods.push(period);
+    }
+
+    dbg!(&periods);
+
+    let res = periods.into_iter().map(|p| p as u64).product::<u64>();
+
+    res
+}
+
+fn print_dot(input: &[(Tag, Module, Vec<Tag>)]) {
+    let mut graph = graph!(strict di id!("g"));
+
+    for (tag, module, outputs) in input {
+        let &tag = tag;
+        let label = match module {
+            Module::Broadcaster => r#""\N""#,
+            Module::FlipFlop(_) => r#""%\N""#,
+            Module::Conjunction(_) => r#""&\N""#,
+        };
+        graph.add_stmt(stmt!(node!(tag; attr!("label", label))));
+        for &output in outputs {
+            graph.add_stmt(stmt!(edge!(node_id!(tag) => node_id!(output))));
+        }
+    }
+
+    let dot = graph.print(&mut PrinterContext::default());
+    println!("{}", dot);
+}
+
 fn solve(input: &str) {
+    let args = std::env::args().collect_vec();
+    let args = args.iter().map(|s| s.as_str()).collect_vec();
+
     let input = parser.parse(input).unwrap();
 
-    let a = solve_a(&input);
-    println!("a: {a}");
+    match &args[1..] {
+        ["a"] => {
+            let a = solve_a(&input);
+            println!("a: {a}");
+        }
+        ["dot"] => {
+            print_dot(&input);
+        }
+        ["b"] => {
+            let b = solve_b(&input);
+            println!("b: {b}");
+        }
+        _ => panic!("invalid arguments"),
+    }
 }
 
 fn main() {
@@ -176,7 +342,7 @@ fn parser<'a>(input: &mut &'a str) -> PResult<Vec<(Tag<'a>, Module<'a>, Vec<Tag<
     let mut module = alt((
         "broadcaster".map(|tag| (tag, Module::Broadcaster)),
         preceded('%', tag).map(|tag| (tag, Module::FlipFlop(PulseValue::Low))),
-        preceded('&', tag).map(|tag| (tag, Module::Conjunction(VecMap::new()))),
+        preceded('&', tag).map(|tag| (tag, Module::Conjunction(Default::default()))),
     ));
 
     let mut outputs = separated(1.., tag, (',', space0));
